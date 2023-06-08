18128af6366a6cdb0f2b3bc74b21a61fb7d20905
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-06-08 17:57:15 -0300

    remove listing trash

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smart_bin/pages/Details_Bin.dart';
+import 'package:smart_bin/widgets/listBins.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,77 +26,9 @@ class _HomePageState extends State<HomePage> {
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))),
       ),
-      body: Padding(
-        padding:
-            const EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 20),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 230,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-          ),
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return DetailsBin(index: index);
-                  },
-                ));
-              },
-              child: Container(
-                width: 160,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.black,
-                  ),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(20),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        'lixeira ${index + 1}',
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22,
-                        ),
-                      ),
-                      Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: [
-                          Image.asset(
-                            'assets/img/bin.png',
-                            width: 80,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 5),
-                            child: Container(
-                              width: 58,
-                              height: 65 * 0.5,
-                              decoration: const BoxDecoration(
-                                  color: Color(0xFF775F19),
-                                  borderRadius: BorderRadius.only(
-                                      bottomRight: Radius.circular(5),
-                                      bottomLeft: Radius.circular(5))),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
+      body: const Padding(
+        padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 20),
+        child: ListBins(),
       ),
     );
   }
